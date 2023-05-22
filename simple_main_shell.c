752c9ac95746fd93a4f6fcc556802bc3d9c424b8
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-22 08:49:42 -0700

    update files

--- simple_main_shell.c
+++ simple_main_shell.c
@@ -9,10 +9,8 @@
  */
 int main(int argc, char **argv, char **envp)
 {
-	char *command = NULL;
-	char **command_args = NULL;
-	int path_count = 0;
-	int exit_status = 0;
+	char *command = NULL, **command_args = NULL;
+	int path_count = 0, exit_status = 0;
 	(void)argc;
 
 	while (1)
@@ -27,21 +25,19 @@ int main(int argc, char **argv, char **envp)
 				free(command);
 				continue;
 			}
-
 			if (!strcmp(command_args[0], "exit") && command_args[1] == NULL)
 				ext_func(command_args, command, exit_status);
 			else if (!strcmp(command_args[0], "env"))
 				get_envi(envp);
 			else
 			{
-				int path_result = path_separator(&command_args[0], envp);
+				int path_result = path_sepetator(&command_args[0], envp);
 
 				exit_status = fork_func(command_args, argv, envp, command,
 						path_count, path_result);
 				if (path_result == 0)
 					free(command_args[0]);
 			}
-
 			free(command_args);
 		}
 		else
@@ -50,9 +46,7 @@ int main(int argc, char **argv, char **envp)
 				write(STDOUT_FILENO, "\n", 1);
 			exit(exit_status);
 		}
-
 		free(command);
 	}
-
 	return (exit_status);
 }
